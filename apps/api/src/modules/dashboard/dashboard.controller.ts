de3e4e5da19b5965f8739f8c7d36235bf57b02a9
Evan ngugi wambui <evanngugi547@gmail.com>
2026-06-23 20:57:51 +0300

    add dashboard functionality

---
+++ apps/api/src/modules/dashboard/dashboard.controller.ts
@@ -0,0 +1,18 @@
+import type { Request, Response, NextFunction } from "express";
+import { dashboardService } from "./dashboard.service.js";
+import { success } from "../../shared/utils/response.js";
+import { asyncHandler } from "../../shared/utils/asyncHandler.js";
+import { logger } from "../../shared/lib/logger.js";
+
+const log = logger.child({ module: "dashboard.controller" });
+
+export const getStats = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    log.debug(
+      { event: "dashboard.stats.requested" },
+      "Fetching dashboard stats",
+    );
+    const stats = await dashboardService.getStats();
+    success(res, stats);
+  },
+);
