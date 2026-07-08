a326059099cf557e0f26bf57332cfc04424148d4
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-08 13:58:40 +0300

    add env types

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/react" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_VAPID_PUBLIC_KEY: string;
+  // Add more custom env variables here...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
