f70989b060091dd30f5c7a25c69cdc227085fa94
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-26 01:53:12 +0300

    update manifest file

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,8 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -21,15 +21,15 @@ export default defineConfig({
       },
 
       manifest: {
-        name: "Wanny's Nails",
-        short_name: "Wannys",
+        name: "Nails by Wanny",
+        short_name: "Wanny Nails",
         description:
-          "Booking management platform purposely built for Kenyan nail salon",
+          "Booking management application purposely built for Nails by Wanny nail salon",
         display: "standalone",
         orientation: "portrait",
         theme_color: "#C084A8",
         background_color: "#FAFAFA",
-        start_url: "/",
+        start_url: "/dashboard",
         scope: "/",
 
         icons: [
@@ -41,7 +41,51 @@ export default defineConfig({
             type: "image/png",
             purpose: "maskable",
           },
+        ],
+       /**
+        * Only 8 screenshots at max
+        * Only jpeg and png
+        * Same form-factor must have same aspect ratio
+        * Width and height within 320px-3840px
+        */
 
+        screenshots: [
+          {
+            src: "/images/dashboard-mobile",
+            sizes: "474x745",
+            type: "image/png",
+            form_factor: "narrow",
+            label: "Mobile view of the dashboard in the booking app",
+          },
+          {
+            src: "/images/bookingpage-mobile",
+            sizes: "472x744",
+            type: "image/png",
+            form_factor: "narrow",
+            label: "Mobile view of bookings in the booking app",
+          },
+        ],
+
+        shortcuts: [
+          {
+            name: "Dashboard",
+            url: "/dashboard"
+          },
+          {
+            name: "Bookings",
+            url: "/bookings"
+          },
+          {
+            name: "Payments",
+            url: "/payments"
+          },
+          {
+            name: "Customers",
+            url: "/customers"
+          }, {
+            name: "Settings",
+            url: "/settings"
+          }
         ],
       },
 
