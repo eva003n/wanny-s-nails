479b1481e9c7d415f082eb0ac4d66945e13a58a1
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-26 04:27:06 +0300

    log env var

--- apps/packages/prisma/seed.ts
+++ apps/packages/prisma/seed.ts
@@ -17,7 +17,7 @@ if(isDevelopment) {
 
 
 const SALT_ROUNDS = 12;
-
+console.log(process.env.DATABASE_URL)
 const adapter = new PrismaPg(process.env.DATABASE_URL!);
 const prisma = new PrismaClient({ adapter });
 
