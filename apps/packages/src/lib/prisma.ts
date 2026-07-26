994c413584b59cacddf857d05bfe6901abf59eaa
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-26 04:42:31 +0300

    fix bug on prismapg connection string

--- apps/packages/src/lib/prisma.ts
+++ apps/packages/src/lib/prisma.ts
@@ -10,7 +10,7 @@ export const  createPrismaClient = (url: string | undefined, env: string = "deve
   if(!url) {
     throw new Error("DATABASE_URL is required")
   }
-  const adapter = new PrismaPg(url);
+  const adapter = new PrismaPg({ connectionString: url });
 
   const prisma = globalThis.prisma ?? new PrismaClient({
     adapter,
