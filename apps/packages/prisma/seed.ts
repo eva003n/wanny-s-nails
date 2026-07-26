be0609c94ad6f0b109f6983733df3bfc875d0914
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-26 04:09:22 +0300

    remove dev code

--- apps/packages/prisma/seed.ts
+++ apps/packages/prisma/seed.ts
@@ -3,12 +3,18 @@ import { fileURLToPath } from "node:url";
 import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+const isDevelopment = process.env.NODE_ENV || "development"
+
+if(isDevelopment) {
+  const dotenv = await import("dotenv")
+  dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+}
+
 
 const SALT_ROUNDS = 12;
 
